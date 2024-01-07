371120eb017300e3d19effda890c1ab2a7df79eb
Jack Blakeston <jackblakeston93@gmail.com>
2024-01-08 00:26:48 +0100

    AS-007: Fix esbuild conflict with firebase

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,12 @@
 import esbuild from 'esbuild';
 import { runStyledScript } from './scripUtils/styledScript.js';
 
+const OPTIONS_TO_FIX_FIREBASE_ESBUILD_CONFLICT = {
+  banner: {
+    js: "const require = (await import('node:module')).createRequire(import.meta.url);const __filename = (await import('node:url')).fileURLToPath(import.meta.url);const __dirname = (await import('node:path')).dirname(__filename);",
+  },
+};
+
 const buildFiles = async () => {
   await esbuild.build({
     entryPoints: ['./src/main.ts'],
@@ -12,6 +18,7 @@ const buildFiles = async () => {
     target: 'node18',
     tsconfig: './tsconfig.json',
     external: ['express', 'mongoose', 'swagger-ui-express'],
+    ...OPTIONS_TO_FIX_FIREBASE_ESBUILD_CONFLICT,
   });
 };
 
